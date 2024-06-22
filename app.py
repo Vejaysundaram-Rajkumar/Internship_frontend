16cd5c635f5be75dd61221a263998877da3e8555
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-06-22 13:07:33 +0530

    Uploading concept
    Now got the video file from the user and an alert is shown as upload sucessful

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'D:\\projects\\Internship_frontend\\uploads'
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def index():
@@ -9,6 +16,42 @@ def index():
 @app.route('/generate')
 def generate():
     return render_template("generate.html")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    try:
+        if request.method == 'POST':
+            print("hi1")
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                return redirect(request.url)
+            print("hi2")
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            print(file.filename)
+            if file.filename == '':
+                print(file.filename)
+                return redirect(request.url)
+            print("hi4")
+            print(file)
+            if file and allowed_file(file.filename):
+                print("ggs")
+                filename = secure_filename(file.filename)
+                print(filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return render_template('generate.html', message="successful")
+        return render_template("generate.html",message="none")
+    except:
+        status= "updation error"
+        message="Upload failure!"
+        return render_template('error.html', error_title=status, error_message=message), 500
+
 
 if __name__ == '__main__':
     app.run()
