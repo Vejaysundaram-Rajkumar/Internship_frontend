cd55798c3a09dcf0172c72033c66a15ec3670db4
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-06-24 14:54:11 +0530

    Update files

--- test.py
+++
@@ -1,8 +0,0 @@
-import shutil
-import os
-
-filename="D:\\projects\\Internship_frontend\\Templates\\error.html"
-target_folder = "D:\\projects\\Internship_frontend"
-
-
-shutil.copy(filename,target_folder)
