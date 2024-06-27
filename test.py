daf629ec4ed8722aeef3aa38f65817a9d51a2053
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-06-27 17:17:52 +0530

    trying SeemlessM4T model

---
+++ test.py
@@ -0,0 +1,4 @@
+import torchaudio
+from transformers import AutoProcessor, SeamlessM4TModel
+processor = AutoProcessor.from_pretrained("facebook/hf-seamless-m4t-medium")
+model = SeamlessM4TModel.from_pretrained("facebook/hf-seamless-m4t-medium")
