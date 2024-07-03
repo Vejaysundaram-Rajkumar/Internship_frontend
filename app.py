b4da8f1135b665a74377ccbcef6c82fada45d96f
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-07-03 09:34:13 +0530

    structure makeover

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import os
 from werkzeug.utils import secure_filename
 import functions
 import Translate
-UPLOAD_FOLDER = 'D:\\projects\\Internship_frontend\\uploads'
-ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov'}
+UPLOAD_FOLDER = 'D:/projects/Internship_frontend/uploads'
 images=os.path.join('static','images')
 upload=os.path.join('uploads')
 app = Flask(__name__)
@@ -17,9 +16,6 @@ genfile_path=''
 folder_path=''
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -46,19 +42,11 @@ def upload_file():
     try:
         global genfile_path,folder_path
         if request.method == 'POST':
-            print("hi1")
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                return redirect(request.url)
-            print("hi2")
             file = request.files['file']
             folder_path = request.form['folderpath']
             option = request.form['option'] 
-            print(option)
-            if file.filename == '':
-                uploaded_files=functions.read_from_file()
-                return render_template("index.html",message="file error",fav_icon=fav_icon,uploaded_files=uploaded_files) 
-            if file and allowed_file(file.filename):
+            #print(option) 
+            if file:
                 filename = secure_filename(file.filename)
                 filepath=os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 print(filepath)
@@ -75,16 +63,13 @@ def upload_file():
                     os.remove(genfile_path)
                 else:
                     uploaded_files=functions.save_file(genfile_path,folder_path)
-                return render_template("index.html",message="successful",filepath=genfile_path,fav_icon=fav_icon, uploaded_files=uploaded_files)
-            else:
-                uploaded_files=functions.read_from_file()
-                return render_template("index.html",message="file error",fav_icon=fav_icon)                
+                return render_template("index.html",message="successful",filepath=genfile_path,fav_icon=fav_icon, uploaded_files=uploaded_files)                
         return render_template("index.html",message="none",fav_icon=fav_icon)
     except Exception as e:
         print(e)
         status= "updation error"
-        message="Upload failure!"
-        return render_template('error.html', error_title=status, error_message=message), 500
+        message=e   
+        return render_template('error.html', error_title=status, error_message=message,fav_icon=fav_icon), 500
 
 
 if __name__ == '__main__':
